fcd8ec36369de1e75036cad985c0c5641080b568
ehsan <ehsan.shirzadi@gmail.com>
2022-06-07 17:21:56 +0430

    added log tools

---
+++ log_tools.py
@@ -0,0 +1,21 @@
+import logging
+LOG_LEVEL = logging.DEBUG
+LOGFORMAT = "  %(log_color)s %(asctime)s, %(levelname)-8s [%(filename)s:%(lineno)d] | %(message)s%(reset)s"
+from colorlog import ColoredFormatter
+logging.root.setLevel(LOG_LEVEL)
+formatter = ColoredFormatter(LOGFORMAT)
+stream = logging.StreamHandler()
+stream.setLevel(LOG_LEVEL)
+stream.setFormatter(formatter)
+log = logging.getLogger('pythonConfig')
+log.setLevel(LOG_LEVEL)
+log.addHandler(stream)
+
+
+
+
+# from systemd.journal import JournalHandler
+# log = logging.getLogger('demo')
+# log.addHandler(JournalHandler())
+# log.setLevel(logging.INFO)
+# log.info("sent to journal")
