53f335c1926f5d576e52e86b993cea5a79ae87c8
ehsan <ehsan.shirzadi@gmail.com>
2022-06-07 17:22:38 +0430

    added try block

--- main.py
+++ main.py
@@ -1,9 +1,21 @@
 from youtube_transcript_api import YouTubeTranscriptApi
+from log_tools import log
+from publics import ExceptionLine
+
+has_persian_sub = "Cd-artSbpXc"
+hasnt_persian_sub = "W9wAfqBd_T0"
 
 
 def get_transcript(video_id):
-    return YouTubeTranscriptApi.get_transcript(video_id, languages=['fa'])
+    try:
+        return YouTubeTranscriptApi.get_transcript(video_id, languages=['fa'])
+    except:
+        log.error(f'An error occurred! {ExceptionLine()}')
+        return None
 
 
-print(get_transcript("8RI0JnB_OaE"))
 
+sub = get_transcript(hasnt_persian_sub)
+if sub is not None:
+    for item in sub:
+        print(item)
