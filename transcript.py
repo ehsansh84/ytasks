85cb13a9ff6d66414f8d826287efbce04171a49d
ehsan <ehsan.shirzadi@gmail.com>
2022-06-08 14:18:11 +0430

    added transcript.py

---
+++ transcript.py
@@ -0,0 +1,16 @@
+from youtube_transcript_api import YouTubeTranscriptApi
+from log_tools import log
+from publics import ExceptionLine, db
+
+
+def get_transcript(video_id):
+    try:
+        return YouTubeTranscriptApi.get_transcript(video_id, languages=['fa'])
+    except:
+        log.error(f'An error occurred! {ExceptionLine()}')
+        return None
+
+col_video = db()['video']
+for item in col_video.find({'subtitle': {'$exists': False}}, {'video_id': 1}):
+    sub = get_transcript(item['video_id'])
+    print(col_video.update_one({'_id': item['_id']}, {'$set': {'subtitle': sub}}))
