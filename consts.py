9bb6af5dd37dc7c468018f9f9cf7b7641f97af73
ehsan <ehsan.shirzadi@gmail.com>
2022-06-08 14:00:45 +0430

    changed db port

--- consts.py
+++ consts.py
@@ -4,5 +4,5 @@ import os
 class consts(object):
     PROJECT_NAME = 'ytasks'
     ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
-    MONGODB_PORT = '27017'
+    MONGODB_PORT = '30012'
     DB_NAME = PROJECT_NAME if os.getenv('DB_NAME') is None else os.getenv('DB_NAME')
