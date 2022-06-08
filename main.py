46752412b8e59c7335c238c22ab822c68e6a595a
ehsan <ehsan.shirzadi@gmail.com>
2022-06-08 14:01:10 +0430

    crawl playlist

--- main.py
+++ main.py
@@ -24,19 +24,45 @@ from pytube import YouTube, Playlist
 
 playlist_link = "https://www.youtube.com/watch?v=W9wAfqBd_T0&list=PLD018AC9B25A23E16"
 
-col_video = db()['vodeo']
+col_video = db()['video']
 print(col_video.count())
 
 video_links = Playlist(playlist_link).video_urls
-print(video_links)
+i = 1
+for link in video_links:
+    if col_video.find_one({"url": link}) is None:
+        video = YouTube(link)
+        print(f"{i}/{len(video_links)} {video.title}")
+        i += 1
+        col_video.insert_one({
+            "url": link,
+            "video_id": video.video_id,
+            "title": video.title,
+            "views": video.views,
+            "author": video.author,
+            "channel_id": video.channel_id,
+            "channel_url": video.channel_url,
+            "keywords": video.keywords,
+            "length": video.length,
+            "publish_date": video.publish_date,
+            "thumbnail_url": video.thumbnail_url,
+            # "captions": video.captions,
+            # "metadata": video.metadata,
+            "description": video.description,
+            "vid_info": video.vid_info,
+            "initial_data": video.initial_data,
+            # "caption_tracks": video.caption_tracks,
+            "sub": []
+        })
+# print(video_links)
 video_titles = []
 start = time()
-for link in video_links:
-    sub = get_transcript(link.split("=")[1])
-    print(link)
-    print(sub is None)
-    # print(link)
-    # print(YouTube(link).title)
-    # video_titles.append(YouTube(link).title)
 
-print(f'Time taken: {time() - start}')
+#     sub = get_transcript(link.split("=")[1])
+#     print(link)
+#     print(sub is None)
+#     # print(link)
+#     # print(YouTube(link).title)
+#     # video_titles.append(YouTube(link).title)
+#
+# print(f'Time taken: {time() - start}')
