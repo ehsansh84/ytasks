c1e0b9387e9ccf249c2dca4736c1502f8f5c7f81
ehsan <ehsan.shirzadi@gmail.com>
2022-06-25 17:00:57 +0430

    added video

--- boot.py
+++ boot.py
@@ -27,4 +27,16 @@ def playlist():
     return render_template('playlist.html', title='Playlists', playlists=data)
 
 
+@app.route('/video')
+def video():
+    col_video = db()['video']
+    result = col_video.find({'subtitle': {'$ne': None}}, {'title': 1, 'views': 1, 'author': 1, 'channel_url': 1, 'url': 1, 'length': 1, 'thumbnail_url': 1}).limit(10)
+    data = []
+    for item in result:
+        data.append(item)
+        print(item)
+    # name = 'Ehsan'
+    return render_template('video.html', title='Videos', data=data)
+
+
 app.run(host='0.0.0.0', port=8001)
