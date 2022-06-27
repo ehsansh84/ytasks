66625f9e0d3568018778c3fdc45798b458fc7f9a
ehsan <ehsan.shirzadi@gmail.com>
2022-06-27 16:02:12 +0430

    added helm chart

--- app.py
+++ app.py
@@ -49,5 +49,5 @@ def video():
 
 
 if __name__ == '__main__':
-    # app.run(host='0.0.0.0', port=8111, debug=True)
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=8080, debug=True)
+    # app.run(debug=True)
